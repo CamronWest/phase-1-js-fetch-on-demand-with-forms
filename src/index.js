fab922e30e7a0bfbc1d728b3048ef7e4f0581324
Camron <cawe219@gmail.com>
2023-01-17 15:10:05 -0500

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
+
+
 const init = () => {
-  
-}
+  const inputForm = document.querySelector('form');
+  inputForm.addEventListener("submit", (event) =>{
+    event.preventDefault();
+    const input = document.querySelector('input#searchByID');
+    
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then((response) => response.json())
+    .then((data) => {
+         const summary = document.querySelector("#movieDetails > p");
+         const title= document.querySelector("#movieDetails > h4");
+
+         title.innerText = data.title;
+         summary.innerText = data.summary;
+    });
+  });
+};
 
 document.addEventListener('DOMContentLoaded', init);
